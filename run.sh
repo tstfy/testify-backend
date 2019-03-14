276d5722a55c8d76c6330023abebea15a366b902
Jae Han John Chung <john.chung@uwaterloo.ca>
2019-03-14 03:52:27 -0400

    pull current branch

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export FLASK_APP=run.py
-git pull origin master
+git pull origin $(git branch | grep \* | cut -d ' ' -f2)
 source /home/testify/testify/bin/activate
 nohup flask run &
 
